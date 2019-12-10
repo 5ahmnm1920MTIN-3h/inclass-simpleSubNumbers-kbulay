695cdf54a851d403b1e0044dcb031efee1cf750a
kevinbulay <kevinbulay@gmx.at>
2019-12-10 16:58:37 +0100

    added Unity Setup from previous project

---
+++ Assets/My Game/Scripts/ComputeNumbers.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public Text result;
+    public InputField inputField1;
+    public InputField inputField2;
+    public Button resetButton;
+    public Button addButton;
+
+    public void SetResult()
+    {
+        // set the result from the AddNumbers method
+        result.text = AddNumbers().ToString();
+
+        // disable InputFields
+        inputField1.interactable = false;
+        inputField2.interactable = false;
+
+        // activate needet Buttons
+        addButton.interactable = false;
+        resetButton.interactable = true;
+    }
+
+    public void Reset()
+    {
+        // reset inputFields
+        inputField1.text = "0";
+        inputField2.text = "0";
+        result.text = "Result";
+
+        // enable inputFields
+        inputField1.interactable = true;
+        inputField2.interactable = true;
+
+        // activate needed Buttons
+        addButton.interactable = true;
+        resetButton.interactable = false;
+
+    }
+    private float AddNumbers()
+    {
+        float tempResult = float.Parse(inputField1.text) + float.Parse(inputField2.text);
+        return tempResult;
+    }
+}
